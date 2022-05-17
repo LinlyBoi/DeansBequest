a6368c8cdfbe3e8fbbadce122a6387d0400cfc0f
linly <libkyy@e.email>
2022-05-17 16:20:56 +0200

    BonsaiTree is PAINJNNNNNNNNN

---
+++ src/ScuffedLinkedList/BonsaiLeaf.java
@@ -0,0 +1,11 @@
+package ScuffedLinkedList;
+
+public class BonsaiLeaf {
+    BonsaiLeaf left;
+    BonsaiLeaf right;
+    int value;
+
+    public BonsaiLeaf(int value) {
+        this.value = value;
+    }
+}
